77e5659f581393314031806ef4e785bc86057bbc
Máté <komate1011@gmail.com>
2024-10-27 16:16:54 +0100

    Canvasban nyitható menü ahol a műhelyeket lehet kiválasztani

--- 3. forduló/script.js
+++ 3. forduló/script.js
@@ -88,6 +88,7 @@ function zoldCheck(img) {
 function torles(context){
     context.clearRect(0, 0, c.width, c.height);
 }
+
 // Méret beállítása a szülő elem méretéhez képest
 function resizeCanvas() {
     // Canvas belső felbontásának igazítása a szülő méretéhez
@@ -97,8 +98,10 @@ function resizeCanvas() {
     /*const scaleX = c.width / eredetiCanvasWidth;
     const scaleY = c.height / eredetiCanvasHeight;
     console.log(scaleX,scaleY)*/
-    let arany=c.width/c.height
+    //Ha magasságban kicsinyítjük az ablakot akkor nagyobb lesz az arany es a kepek
+    //let arany=c.width/c.height
     muhelyek.forEach(muhely => {
+        muhely.radius=muhely.eredetiRadius * arany
         muhely.width = muhely.eredetiWidth * arany;
         muhely.height = muhely.eredetiHeight * arany;
         muhely.draw(ctx)
@@ -107,17 +110,88 @@ function resizeCanvas() {
     // Ha szükséges, itt újrarajzolhatod a canvas tartalmát, mivel méretezéskor a rajz eltűnik
     // draw(); // Ezt a függvényt hozd létre a rajzolási tartalom megjelenítéséhez
 }
+function canvasRajzolas(){
+    torles(ctx);
+    menuRajzolas(ctx);
+    menuGombRajzolas();
+}
 
+//####################################################
+//Menü függvényei
+function menuRajzolas(ctx){
+    ctx.beginPath();
+    ctx.strokeStyle="black";
+    ctx.lineWidth=2;
+    ctx.rect(menuX,menuY,menuWidth,-menuAktualisHeight) // menü területe
+    ctx.stroke();
+}
+
+function menuGombRajzolas(){
+
+    ctx.beginPath();
+    ctx.rect(menuGombX,menuGombY,menuGombWidth,menuGombHeight) //gomb rajzolása
+    ctx.stroke();
+
+    //Nyíl rajzolása
+    ctx.beginPath();
+    ctx.moveTo(menuGombX+menuGombWidth/2, menuGombY+10); // felső középpont
+    ctx.lineTo(menuGombX+10, menuGombY+40);  // bal felső pont
+    ctx.lineTo(menuGombX+(menuGombWidth-10), menuGombY+40);  // jobb felső pont
+    ctx.fillStyle = 'black'; // nyíl színe
+    ctx.fill();
+    ctx.closePath();
+}
+function menuNyitas(){
+
+    if (menuAktualisHeight < menuHeight) {
+        menuAktualisHeight += 10; // Animációs lépés mérete
+        menuGombY = c.height-menuGombHeight-menuAktualisHeight; //Gomb y koordinátákjának feljebb tolása
+        canvasRajzolas();
+        requestAnimationFrame(menuNyitas); // Következő animációs lépés
+    } else {
+        menuAktualisHeight = menuHeight; // Biztosítjuk, hogy pontosan a max szélesség legyen
+        console.log("Kinyitás vége")
+        muhelyMenu()
+        animacio = false; // Animáció vége
+    }
+    
+
+}
+
+
+function menuBezaras(ctx){
+    if (menuAktualisHeight > 0) {
+        menuAktualisHeight -= 10; // Animációs lépés mérete
+        menuGombY = c.height-menuGombHeight-menuAktualisHeight; //Gomb y koordinátákjának feljebb tolása
+        canvasRajzolas();
+        requestAnimationFrame(menuBezaras); // Következő animációs lépés
+    } else {
+        menuAktualisHeight = 0; // Biztosítjuk, hogy pontosan a max szélesség legyen
+        animacio = false; // Animáció vége
+    }
+
+}
+
+function muhelyMenu(){
+    let zold=document.getElementById("allomasZold");
+    let kek=document.getElementById("allomasKek");
+    let narancs=document.getElementById("allomasNarancs");
+    ctx.drawImage(zold,menuWidth/3-150,menuY-110,100,100)
+    ctx.drawImage(kek,menuWidth/3*2-150,menuY-110,100,100)
+    ctx.drawImage(narancs,menuWidth-150,menuY-110,100,100)
+
+}
 const c = document.getElementById("jatek");
 const ctx = c.getContext("2d");
 
 const windowHeight=window.innerHeight;
 const windowWidth=window.innerWidth;
-
+//const cavasRect = c.getBoundingClientRect();
 c.style.border="1px black solid"
 const eredetiCanvasWidth=c.width;
 const eredetiCanvasHeight=c.height;
 
+var animacio = false;
 var muhelyek=[];
 var mouseDown=false;
 var kekOn = false;
@@ -128,13 +202,27 @@ var mozgatott;
 /*c.width=windowWidth-700;
 c.height=windowHeight-300;*/
 
+
 // Az ablak méretezésekor automatikus canvas átméretezés
 window.addEventListener('resize', resizeCanvas);
 
-// Kezdeti méretbeállítás betöltéskor
+// Kezdeti méretbeállítás betöltéskor, töröl mindent
 resizeCanvas();
 
-//var cavasRect = c.getBoundingClientRect();
+const menuWidth = c.width
+const menuHeight = 125;
+var menuAktualisHeight = 0;
+const menuGombWidth = 50;
+const menuGombHeight = 50
+const menuX = 0
+var menuY = c.height
+var menuGombX = c.width/2-menuGombWidth/2;
+var menuGombY = c.height-menuGombHeight-menuAktualisHeight;
+var menuOpen=false;
+
+//Menu ahol ki lehet választani a műhelyeket
+
+canvasRajzolas();
 
 
 //Mozgatható terület vaan mit javítani rajta
@@ -203,6 +291,7 @@ c.addEventListener("mousemove",(event)=>{
         mozgatott.mozgas(ctx,event)
     }
 })
+
 //Ha felengedi az egeret a műhely mozgatása is álljon meg
 c.addEventListener("mouseup",function (event){
     mouseDown=false;
@@ -212,7 +301,7 @@ c.addEventListener('click', function (event) {
     //console.log(event)
     let x = event.offsetX;
     let y = event.offsetY;
-
+    //Műhely lehelyezés
     if (zoldOn == true) {
         let img = document.getElementById("allomasZold");
         img.classList.remove("selected");
@@ -238,8 +327,18 @@ c.addEventListener('click', function (event) {
         kekOn=false;
     }
     
+    //Menu nyitogatás
+    if (x>=menuGombX && x<=menuGombX+menuGombWidth && 
+        y >= menuGombY && y<= menuGombY+menuGombHeight){
+            animacio = true; // Animáció indítása
+            if (!menuOpen) {
+                menuNyitas(); // Menü nyitása
+            } else {
+                menuBezaras(); // Menü zárása
+            }
+            menuOpen = !menuOpen; // Menü állapotának váltása
+        }
+
+
 }, false);
-/*var xPos = event.clientX - cavasRect.left; 
-var yPos = event.clientY - cavasRect.top; 
-console.log(xPos,yPos)*/
 
